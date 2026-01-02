cfd103d94148e9a03b080364c4207caa72f17453
NashifSR <sr.nashif@gmail.com>
2026-01-02 12:06:02 +0600

    new data

--- index.js
+++ index.js
@@ -1,30 +1,76 @@
 const express = require("express"); // 1. Import the whole module
-const studentData = require('./studentData.json')
-const cors = require('cors')
+const studentData = require("./studentData.json");
+const userData = require("./userData.json");
+const cors = require("cors");
 const app = express(); // 2. Initialize the app
 const port = 5000;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello world, i am here");
+let users = []; // replace with real DB later
+
+app.get("/", (req, res) => {
+  res.send("You have tried to enter the hole");
+});
+// ------------------ Students --------------------
+app.get("/studentData", (req, res) => {
+    // console.log(studentData)
+    res.send(studentData);
 });
 
-app.get('/studentData', (req, res) =>{
-    console.log(studentData)
+app.get("/studentData/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  // const user = studentData.find( studentInfo => console.log(studentInfo.id, id) )
+  const user = studentData.find((studentInfo) => studentInfo.id === id);
+  res.send(user);
+});
+
+app.post("/studentData", (req, res) => {
+    const student = req.body;
+    
+  console.log("Received student data:", student);
+  
+  // Example: store in array
+  studentData.push(student);
+
+  res.status(201).json({
+      message: "Student data received successfully",
+      data: student,
+    });
+});
+// ------------------ Students --------------------
+
+app.get("/studentData", (req, res) => {
+    // console.log(studentData)
     res.send(studentData);
-})
+});
 
-app.get('/studentData/:id', (req, res) =>{
-    const id = parseInt(req.params.id);
-    // const user = studentData.find( studentInfo => console.log(studentInfo.id, id) )
-    const user = studentData.find( studentInfo => studentInfo.id === id )
-    res.send(user)
-})
+//------------------- UserData -------------------
 
+
+app.post("/userData", (req, res) => {
+  const user = req.body;
+
+  if (!user.uid || !user.email) {
+    return res.status(400).json({ message: "UID and email required" });
+  }
+
+  // store user (in real app, use DB)
+  users.push(user);
+
+  res.status(201).json({ message: "User saved", user });
+});
+
+app.get("/userData", (req, res) => {
+    // console.log(studentData)
+    res.send(userData);
+});
+
+//------------------- UserData -------------------
 
 //---------------------------------------------------------
 // 3. Use .listen(), not .listenerCount()
 app.listen(port, () => {
-    console.log(`My server is running on ${port}`);
+  console.log(`My server is running on ${port}`);
 });
